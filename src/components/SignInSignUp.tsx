3ce459d248f1888229950d7a06c881dfe16cc06b
liusong <2227616052@qq.com>
2023-03-12 11:38:19 +0800

    init sign-in and sign-up screen

---
+++ src/components/SignInSignUp.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class SignInSignUp extends Component {
+  render() {
+    return (
+      <View>
+        <Text>SignInSignUp</Text>
+      </View>
+    )
+  }
+}
